666875c2332fff6094e915c6d3a73fa44288d446
lavaspoon <lavaspoon@naver.com>
2021-11-15 16:20:16 +0900

    상품 상세, 등록 폼 완성

--- item-service/src/main/java/hello/itemservice/domain/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/domain/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -36,6 +37,16 @@ public class BasicItemController {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
         return "basic/item";
+    }
+    //상품 등록
+    @GetMapping("/add")
+    public String addForm(){
+        return "/basic/addForm";
+    }
+    //상품 등록
+    @PostMapping("/add")
+    public String save(){
+        return "/basic/addForm";
     }
     /**
      * 테스트용
