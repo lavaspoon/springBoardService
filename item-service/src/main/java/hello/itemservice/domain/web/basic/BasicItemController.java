a83384131a502f7d809d51b6037321547c19833a
lavaspoon <lavaspoon@naver.com>
2021-11-16 16:05:09 +0900

    상품 수정 기능 구현 및 등록버튼 라다이렉트 버그 수정

--- item-service/src/main/java/hello/itemservice/domain/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/domain/web/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -73,11 +74,25 @@ public class BasicItemController {
     }
     //상품 등록 v4
     @PostMapping("/add")
-    public String addItemV4(Item item){
-        itemRepository.save(item);
-        return "/basic/item";
+    public String addItemV4(Item item, RedirectAttributes redirectAttributes){
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
     }
-
+    //상품 수정 페이지
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+    //상품 수정
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item){
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
     /**
      * 테스트용
      */
